7ebb834ba11339a539074b81eca7588b8bbde64c
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-11-30 20:45:06 +0000

    added tasks and pings _count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_182939) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_204311) do
   create_table "friend_requests", force: :cascade do |t|
     t.integer "recipient_id", null: false
     t.integer "sender_id", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_182939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "pings_count", default: 0
+    t.integer "tasks_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
